4c2df70212ae721b0fd61116cbda47fbb404220f
Antonio Sanchez <asanton@gmail.com>
2021-02-18 14:39:47 +0100

    Practica 2 Unir.Automatizacion

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,10 +7,11 @@ variable "location" {
 variable "vm_size" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+  default = "Standard_A2_v2" # 4 GB, 2 CPU    https://docs.microsoft.com/en-us/azure/cloud-services/cloud-services-sizes-specs
+  #default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
 }
 
 variable "vms" {
   description = "Maquinas Virtuales a crear"
-  default = ["master","nfs","worker01","worker02"]
+  default = ["master","worker01"]  // default = ["master","nfs","worker01","worker02"] Tenemos limitacion y solo el master necesita 2 cpu
 }
